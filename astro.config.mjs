111926c0b254fd57a147bbe59275b83394b0fa5c
Daniel Rojo <danielrojo@gmail.com>
2024-12-10 09:05:14 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,16 @@ import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
 import icon from "astro-icon"
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
+
   integrations: [mdx(), icon(), tailwind({
     applyBaseStyles: false,
   }), compress()],
+
+  output: 'server',
+  adapter: vercel(),
 })
